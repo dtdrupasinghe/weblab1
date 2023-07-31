71a9883480d43e9a0e69e8739d83c9dca28ba7d4
Thisara Rupasinghe <136635924+dtdrupasinghe@users.noreply.github.com>
2023-07-31 14:21:48 +0530

    Add files via upload

---
+++ Weblab/Tute 11 2023-07-20/exce_02.php
@@ -0,0 +1,7 @@
+<!-- http://localhost/PHP1/exce_02.php -->
+<?php
+// Using a for loop to display count from 5 to 15
+for ($i = 5; $i <= 15; $i++) {
+    echo $i . "\n";
+}
+?>
---
+++ Weblab/Tute 11 2023-07-20/exce_05.php
@@ -0,0 +1,11 @@
+<!-- http://localhost/PHP1/exce_05.php -->
+
+<?php
+
+$fruits = array("Apple", "Banana", "Orange", "Grapes", "Mango");
+
+
+foreach ($fruits as $fruit) {
+    echo $fruit . "\n";
+}
+?>
